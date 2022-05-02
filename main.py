d0585ede12a90d53a039c2da2841899d9f1c3c35
Arthur <KarimullinArthur@engineer.com>
2022-05-02 12:07:58 +0500

    more clear code and some comments

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ import getch
 import time
 import colorama
 
-text = "Hello world!" 
+text = '''My notion was that you had been (Before she had this fit) An obstacle that
+came between Him, and ourselves, and it. Don't let him know she liked them''' 
+# some text
 
 print(text,end='\r')
 
+# main vars
 lenght = len(text)
 mistake = 0
 right = 0
@@ -15,15 +18,17 @@ right = 0
 for x in range(lenght):
     char = getch.getch()
 
-    if x  == 0:
+    if x == 0:
         start = time.time()
-    if char == text[x]:
+
+    elif char == text[x]:
         print(colorama.Fore.GREEN,char, sep='', end='', flush=True)
         right += 1
     else:
         print(colorama.Fore.RED,char,   sep='', end='', flush=True )
         mistake += 1
 
+# calc
 finish = time.time()-start
 finish = round(finish,2)
 
@@ -33,6 +38,7 @@ speed = round(speed,2)
 mistakePercent = mistake/lenght*100
 mistakePercent = round(mistakePercent,2)
 
+# prints
 print(colorama.Style.RESET_ALL)
 
 print(f"\n\nmistake {mistakePercent}%")
