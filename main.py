8656144cf75f1bb10ba0429f1e57d9d0257fa30e
Arthur <KarimullinArthur@engineer.com>
2022-05-05 18:16:57 +0500

    вроде всё ок, только нужно что бы цикл останавливaлся вовремя

--- main.py
+++ main.py
@@ -4,38 +4,36 @@ import getch
 import time
 import colorama
 
-text = '''Mahomet was born at Mecca in Arabia, in the year of our Lord 571. He was of the tribe of the Korashites, esteemed the noblest in all that country, and was descended in a direct line, from Pher Koraish, the first founder of it.''' 
 # some text
+text = '''Mahomet was born at Mecca in Arabia, in the year of our Lord 571.''' 
 
 # main vars
 lenght = len(text)
 mistake = 0
 right = 0
 
-text1 = text.split(',')
+textPart = text.split(',')
 
-print(text1[0],end='\r')
-for sentence in range(len(text1)):
+print(textPart[0],end='\r')
+for sentence in range(len(textPart)):
 
     for charStep in range(lenght):
-    
-        if charStep == len(text1[sentence]):
-            for b in text1:
-                a = len(text1[sentence])
-                print('\b'*a,b,'\r',end='')
- 
         char = getch.getch()
-    
+
+        if charStep == 0:
+           start = time.time()
+       
+        if charStep == len(textPart[sentence]):
+            for x in range(len(textPart)):
+                a = len(textPart[sentence])
+                print('\b'*a,textPart[x],'\r',end='',flush=True)
+
         if char == text[charStep]:
-    
-            if charStep == 0:
-                start = time.time()
-    
             print(colorama.Fore.GREEN,char, colorama.Style.RESET_ALL, sep='', end='', flush=True)
             right += 1
     
         else:
-            print(colorama.Fore.RED,char,   colorama.Style.RESET_ALL, sep='', end='', flush=True )
+            print(colorama.Fore.RED,char,   colorama.Style.RESET_ALL, sep='', end='', flush=True)
             mistake += 1
 
 # calc
